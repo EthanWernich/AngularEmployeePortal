21ad9286abb583937a324afdff88c5163a0b07d4
bsar <anthony@baruchstudio.co.za@gmail.com>
2022-08-15 10:17:06 +0200

    Project Commit

---
+++ src/app/department/add-edit/add-edit.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit,Input } from '@angular/core';
+import { Subscription } from 'rxjs';
+import {SharedService} from 'src/app/shared.service';
+
+@Component({
+  selector: 'app-add-edit',
+  templateUrl: './add-edit.component.html',
+  styleUrls: ['./add-edit.component.css']
+})
+export class AddEditComponent implements OnInit {
+
+  public DepartmentId:string ;
+  public DepartmentName:string;
+  public loading : Subscription;
+  constructor(private service:SharedService) { }
+
+  @Input() dep:any;
+
+
+  ngOnInit(): void {
+    this.DepartmentId=this.dep.DepartmentId;
+    this.DepartmentName=this.dep.DepartmentName;
+  }
+
+  addDepartment(){
+    var val = {DepartmentId:this.DepartmentId,
+                DepartmentName:this.DepartmentName};
+                this.loading = this.service.addDepartment(val).subscribe(res=>{
+      alert(res.toString());
+    });
+  }
+
+  updateDepartment(){
+    var val = {DepartmentId:this.DepartmentId,
+      DepartmentName:this.DepartmentName};
+    this.service.updateDepartment(val).subscribe(res=>{
+    alert(res.toString());
+    });
+  }
+
+}
