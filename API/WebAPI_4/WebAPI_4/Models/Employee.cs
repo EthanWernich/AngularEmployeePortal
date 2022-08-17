b5f37dc658942d0bdd0bfefdec15b1c7a1558fbd
bsew <wernichethan@gmail.com>
2022-08-17 11:09:21 +0200

    API - Replaced data layer with EntityFramework Methodology

--- API/WebAPI_4/WebAPI_4/Models/Employee.cs
+++ API/WebAPI_4/WebAPI_4/Models/Employee.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebAPI_4.Models
 {
     public class Employee
     {
         public int EmployeeId { get; set; }
-        public string EmployeeName { get; set; }
-        public string Department { get; set; }
-        public string DateOfJoining { get; set; }
-        public string PhotoFileName{ get; set; }
 
+        [Required]
+        [MaxLength(50)]
+
+        public string EmployeeName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Department { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string DateOfJoining { get; set; }
+
+     
+        public string PhotoFileName{ get; set; }
+     
     }
 }
